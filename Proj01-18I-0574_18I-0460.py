4139eb768fabc130afd71de2baef039c5300c88b
Momin <mominsalar3@gmail.com>
2021-04-28 19:36:32 +0500

    adding required files

---
+++ Proj01-18I-0574_18I-0460.py
@@ -0,0 +1,14 @@
+import numpy
+import pandas as pd
+print("hello")
+print("1234")
+print("hi nerd")
+
+
+print("helo")
+
+a = numpy.array([1, 2, 3, 4])
+a = [1, 2, 3, 4]
+print(a)
+
+pd.read_table('./Data/u.user')
